48ace48718b7a4aafc91fab3afca5e2fafbe2973
Jason Shelley <jshelley@csr.com.au>
2018-09-12 21:18:28 +1000

    Add project files.

---
+++ Dialogs/BasicLuisDialog.cs
@@ -0,0 +1,175 @@
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.Linq;
+
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+
+namespace Microsoft.Bot.Sample.LuisBot
+{
+    // For more information about this template visit http://aka.ms/azurebots-csharp-luis
+    [Serializable]
+    public class BasicLuisDialog : LuisDialog<object>
+    {
+        private static Dictionary<DateTime, string> roster = new Dictionary<DateTime, string>() {
+            {   new DateTime(   2018    ,   9   ,   17  )   ,   "	Matt	"    }   ,
+{   new DateTime(   2018    ,   9   ,   24  )   ,   "	Michael	" }   ,
+{   new DateTime(   2018    ,   10  ,   1   )   ,   "	Doug	"    }   ,
+{   new DateTime(   2018    ,   10  ,   8   )   ,   "	Will	"    }   ,
+{   new DateTime(   2018    ,   10  ,   15  )   ,   "	Hazz	"    }   ,
+{   new DateTime(   2018    ,   10  ,   22  )   ,   "	Jay	" }   ,
+{   new DateTime(   2018    ,   10  ,   29  )   ,   "	Jase	"    }   ,
+{   new DateTime(   2018    ,   11  ,   5   )   ,   "	Rick	"    }   ,
+{   new DateTime(   2018    ,   11  ,   12  )   ,   "	Ben T	"   }   ,
+{   new DateTime(   2018    ,   11  ,   19  )   ,   "	Nick	"    }   ,
+{   new DateTime(   2018    ,   11  ,   26  )   ,   "	Tom	" }   ,
+{   new DateTime(   2018    ,   12  ,   3   )   ,   "	Scott	"   }   ,
+{   new DateTime(   2018    ,   12  ,   10  )   ,   "	PK	"  }   ,
+{   new DateTime(   2018    ,   12  ,   17  )   ,   "	Ben R	"   }   ,
+{   new DateTime(   2018    ,   12  ,   24  )   ,   "	0	"   }   ,
+{   new DateTime(   2018    ,   12  ,   31  )   ,   "	0	"   }   ,
+{   new DateTime(   2019    ,   1   ,   7   )   ,   "	Gaz	" }   ,
+{   new DateTime(   2019    ,   1   ,   14  )   ,   "	Matt	"    }   ,
+{   new DateTime(   2019    ,   1   ,   21  )   ,   "	Michael	" }   ,
+{   new DateTime(   2019    ,   1   ,   28  )   ,   "	Doug	"    }   ,
+{   new DateTime(   2019    ,   2   ,   4   )   ,   "	Will	"    }   ,
+{   new DateTime(   2019    ,   2   ,   11  )   ,   "	Hazz	"    }   ,
+{   new DateTime(   2019    ,   2   ,   18  )   ,   "	Jay	" }   ,
+{   new DateTime(   2019    ,   2   ,   25  )   ,   "	Jase	"    }   ,
+{   new DateTime(   2019    ,   3   ,   4   )   ,   "	Rick	"    }   ,
+{   new DateTime(   2019    ,   3   ,   11  )   ,   "	Ben T	"   }   ,
+{   new DateTime(   2019    ,   3   ,   18  )   ,   "	Nick	"    }   ,
+{   new DateTime(   2019    ,   3   ,   25  )   ,   "	Tom	" }   ,
+{   new DateTime(   2019    ,   4   ,   1   )   ,   "	Scott	"   }   ,
+{   new DateTime(   2019    ,   4   ,   8   )   ,   "	PK	"  }   ,
+{   new DateTime(   2019    ,   4   ,   15  )   ,   "	Ben R	"   }   ,
+{   new DateTime(   2019    ,   4   ,   22  )   ,   "	Gaz	" }   ,
+{   new DateTime(   2019    ,   4   ,   29  )   ,   "	Matt	"    }   ,
+{   new DateTime(   2019    ,   5   ,   6   )   ,   "	Michael	" }   ,
+{   new DateTime(   2019    ,   5   ,   13  )   ,   "	Doug	"    }   ,
+{   new DateTime(   2019    ,   5   ,   20  )   ,   "	Will	"    }   ,
+{   new DateTime(   2019    ,   5   ,   27  )   ,   "	Hazz	"    }   ,
+{   new DateTime(   2019    ,   6   ,   3   )   ,   "	Jay	" }   ,
+{   new DateTime(   2019    ,   6   ,   10  )   ,   "	Jase	"    }   ,
+{   new DateTime(   2019    ,   6   ,   17  )   ,   "	Rick	"    }   ,
+{   new DateTime(   2019    ,   6   ,   24  )   ,   "	Ben T	"   }   ,
+{   new DateTime(   2019    ,   7   ,   1   )   ,   "	Nick	"    }   ,
+{   new DateTime(   2019    ,   7   ,   8   )   ,   "	Tom	" }   ,
+{   new DateTime(   2019    ,   7   ,   15  )   ,   "	Scott	"   }   ,
+{   new DateTime(   2019    ,   7   ,   22  )   ,   "	PK	"  }   ,
+{   new DateTime(   2019    ,   7   ,   29  )   ,   "	Ben R	"   }   ,
+{   new DateTime(   2019    ,   8   ,   5   )   ,   "	Gaz	" }   ,
+{   new DateTime(   2019    ,   8   ,   12  )   ,   "	Matt	"    }   ,
+{   new DateTime(   2019    ,   8   ,   19  )   ,   "	Michael	" }   ,
+{   new DateTime(   2019    ,   8   ,   26  )   ,   "	Doug	"    }   ,
+{   new DateTime(   2019    ,   9   ,   2   )   ,   "	Will	"    }   ,
+{   new DateTime(   2019    ,   9   ,   9   )   ,   "	Hazz	"    }
+        };
+
+
+        public BasicLuisDialog() : base(new LuisService(new LuisModelAttribute(
+            ConfigurationManager.AppSettings["LuisAppId"], 
+            ConfigurationManager.AppSettings["LuisAPIKey"], 
+            domain: ConfigurationManager.AppSettings["LuisAPIHostName"])))
+        {
+            var roster = new Dictionary<DateTime, string>();
+            var value = DateTime.Now;
+            var oneweek = TimeSpan.FromDays(7);
+            var ff = roster.Keys.Select(k => new { start = k, end = k + oneweek }).First(k => k.start <= value && k.end > value);
+
+        }
+
+
+
+        [LuisIntent("None")]
+        public async Task NoneIntent(IDialogContext context, LuisResult result)
+        {
+            await this.ShowLuisResult(context, result);
+        }
+
+        // Go to https://luis.ai and create a new intent, then train/publish your luis app.
+        // Finally replace "Greeting" with the name of your newly created intent in the following handler
+        [LuisIntent("Greeting")]
+        public async Task GreetingIntent(IDialogContext context, LuisResult result)
+        {
+            await this.ShowLuisResult(context, result);
+        }
+
+        [LuisIntent("Cancel")]
+        public async Task CancelIntent(IDialogContext context, LuisResult result)
+        {
+            await this.ShowLuisResult(context, result);
+        }
+
+        [LuisIntent("Help")]
+        public async Task HelpIntent(IDialogContext context, LuisResult result)
+        {
+            await this.ShowLuisResult(context, result);
+        }
+        
+        [LuisIntent("Roster.WhosUp")]
+        public async Task WhosUpIntent(IDialogContext context, LuisResult result)
+        {
+            await this.ShowLuisResult(context, result);
+        }
+
+        private async Task ShowLuisResult(IDialogContext context, LuisResult result) 
+        {
+            var intent = result.TopScoringIntent;
+            await context.PostAsync($"You have reached {intent.Intent}. You said: {result.Query}");
+            foreach (var entity in result.Entities) {
+                await context.PostAsync($"{entity.Entity}: {entity.Type}");
+            }
+            
+            var dateRange = result.Entities.FirstOrDefault(e => e.Type == "builtin.datetimeV2.daterange");
+            if (dateRange != null)
+            {
+
+                dynamic range = dateRange.Resolution.Values.First() as List<object>;
+
+                var min = range.Start;
+                var max = range.End;
+
+                await context.PostAsync($"{min} -> {max}");
+            }
+            
+            context.Wait(MessageReceived);
+        }
+
+        public class DateRange
+        {
+            public string TimeX { get; set; }
+            public string Type { get; set; }
+            public DateTime Start { get; set; }
+            public DateTime End { get; set; }
+        }
+        public class Date
+        {
+            public string TimeX { get; set; }
+            public string Type { get; set; }
+            public DateTime Value { get; set; }
+
+            public static implicit operator Date(List<object> list)
+            {
+                var values = list.First() as Dictionary<string, object>;
+
+                var date = new Date()
+                {
+                    TimeX = values["timex"].ToString(),
+                    Type = values["type"].ToString(),
+                    Value = DateTime.Parse(values["value"].ToString())
+                };
+
+                return date;
+            }
+
+            public override string ToString()
+            {
+                return $"TimeX: {TimeX}, Type: {Type}, Value: {Value}";
+            }
+        }
+    }
+}
---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=rosterbot-Web-Deploy.pubxml -p:Password=BS8mHSSw4ydk5l7nltCgvbln3jeuQH9CWqmEPl9dr2bblrTvYdihq2HzTQue
+
